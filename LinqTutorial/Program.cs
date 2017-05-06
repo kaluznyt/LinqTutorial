86052a7cae81104d13169a75900bd94ad012bc3b
kaluznyt <tkaluzny@outlook.com>
2017-05-06 20:44:23 +0100

    Concat

--- LinqTutorial/Program.cs
+++ LinqTutorial/Program.cs
@@ -52,13 +52,364 @@ namespace LinqTutorial
             this.Aggregate();
             this.Average();
             this.Count();
+            this.Max();
+            this.Sum();
+            this.ElementAt();
+            this.FirstOrDefault();
+            this.LastOrDefault();
+            this.SingleOrDefault();
+            this.SequenceEqual();
+            this.Concat();
+            this.DefaultIfEmpty();
         }
 
+        // The DefaultIfEmpty() method returns a new collection with the default value if the given 
+        // collection on which DefaultIfEmpty() is invoked is empty.
+        private void DefaultIfEmpty()
+        {
+
+        }
+
+        // The Concat() method appends two sequences of the same type and returns a new sequence(collection).
+        private void Concat()
+        {
+            var collection1 = new List<string>() { "One", "Two", "Three" };
+            var collection2 = new List<string>() { "Five", "Six" };
+
+            var collection3 = collection1.Concat(collection2);
+
+            foreach (string str in collection3) Console.WriteLine(str);
+        }
+
+        // There is only one equality operator: SequenceEqual.The SequenceEqual method checks whether the number of 
+        // elements, value of each element and order of elements in two collections are equal or not.
+        // If the collection contains elements of primitive data types then it compares the values and number of 
+        // elements, whereas collection with complex type elements, checks the references of the objects.So, 
+        // if the objects have the same reference then they considered as equal otherwise they are considered not equal.
+        private void SequenceEqual()
+        {
+            var strList1 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+            var strList2 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+            var isEqual = strList1.SequenceEqual(strList2); // returns true
+
+            Console.WriteLine(isEqual);
+
+            var strList3 = new List<string>() { "Two", "Three", "Four", "Three" };
+
+
+            isEqual = strList1.SequenceEqual(strList3); // returns false
+            Console.WriteLine(isEqual);
+
+            var std = new Student() { StudentID = 1, StudentName = "Bill" };
+
+            var studentList1 = new List<Student>() { std };
+
+            var studentList2 = new List<Student>() { std };
+
+            isEqual = studentList1.SequenceEqual(studentList2); // returns true
+
+            var std1 = new Student() { StudentID = 1, StudentName = "Bill" };
+            var std2 = new Student() { StudentID = 1, StudentName = "Bill" };
+
+            var studentList3 = new List<Student>() { std1 };
+
+            var studentList4 = new List<Student>() { std2 };
+
+            isEqual = studentList3.SequenceEqual(studentList4);// returns false
+        }
+
+
+        // Single Returns the only element from a collection, or the only element that satisfies a condition.
+        // If Single() found no elements or more than one elements in the collection then throws InvalidOperationException.
+        // SingleOrDefault The same as Single, except that it returns a default value of a specified generic type, 
+        // instead of throwing an exception if no element found for the specified condition.However, it will thrown
+        // InvalidOperationException if it found more than one element for the specified condition in the collection.
+        private void SingleOrDefault()
+        {
+            var oneElementList = new List<int>() { 7 };
+            var intList = new List<int>() { 7, 10, 21, 30, 45, 50, 87 };
+            IList<string> strList = new List<string>() { null, "Two", "Three", "Four", "Five" };
+            IList<string> emptyList = new List<string>();
+
+            Console.WriteLine("The only element in oneElementList: {0}", oneElementList.Single());
+            Console.WriteLine("The only element in oneElementList: {0}", oneElementList.SingleOrDefault());
+
+            Console.WriteLine("Element in emptyList: {0}", emptyList.SingleOrDefault());
+
+            Console.WriteLine("The only element which is less than 10 in intList: {0}", intList.Single(i => i < 10));
+
+            // Followings throw an exception
+            try
+            {
+                Console.WriteLine("The only Element in intList: {0}", intList.Single());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                Console.WriteLine("The only Element in intList: {0}", intList.SingleOrDefault());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                Console.WriteLine("The only Element in emptyList: {0}", emptyList.Single());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            // following throws error because list contains more than one element which is less than 100
+            try
+            {
+                Console.WriteLine("Element less than 100 in intList: {0}", intList.Single(i => i < 100));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            // following throws error because list contains more than one element which is less than 100
+            try
+            {
+                Console.WriteLine("Element less than 100 in intList: {0}", intList.SingleOrDefault(i => i < 100));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            // following throws error because list contains more than one elements
+            try
+            {
+                Console.WriteLine("The only Element in intList: {0}", intList.Single());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            // following throws error because list contains more than one elements
+            try
+            {
+                Console.WriteLine("The only Element in intList: {0}", intList.SingleOrDefault());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            // following throws error because list does not contains any element
+            try
+            {
+                Console.WriteLine("The only Element in emptyList: {0}", emptyList.Single());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        // Last and LastOrDefault has two overload methods. One overload method doesn't take any input parameter and
+        // returns last element from the collection. Second overload method takes a lambda expression to specify a
+        // condition and returns last element that satisfies the specified condition.
+        // Last Returns the last element from a collection, or the last element that satisfies a condition
+        // LastOrDefault   Returns the last element from a collection, or the last element that satisfies a condition.
+        // Returns a default value if no such element exists.
+        private void LastOrDefault()
+        {
+            var intList = new List<int>() { 7, 10, 21, 30, 45, 50, 87 };
+            var strList = new List<string>() { null, "Two", "Three", "Four", "Five" };
+            var emptyList = new List<string>();
+
+            Console.WriteLine("Last Element in intList: {0}", intList.Last());
+
+            Console.WriteLine("Last Even Element in intList: {0}", intList.Last(i => i % 2 == 0));
+
+            Console.WriteLine("Last Element in strList: {0}", strList.Last());
+
+            Console.WriteLine("emptyList.Last() throws an InvalidOperationException");
+            Console.WriteLine("-------------------------------------------------------------");
+            try
+            {
+                Console.WriteLine(emptyList.Last());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Last Element in intList: {0}", intList.LastOrDefault());
+
+            Console.WriteLine("Last Even Element in intList: {0}",
+                intList.LastOrDefault(i => i % 2 == 0));
+
+            Console.WriteLine("Last Element in strList: {0}", strList.LastOrDefault());
+
+            Console.WriteLine("Last Element in emptyList: {0}", emptyList.LastOrDefault());
+
+            // Be careful while specifying condition in Last() or LastOrDefault(). Last() will throw an exception if a collection does not include any element that satisfies the specified condition or includes null element.
+            // If a collection includes null element then LastOrDefault() throws an exception while evaluting the specified condition.The following example demonstrates this.
+            try
+            {
+                Console.WriteLine("Last Element which is greater than 250 in intList: {0}", intList.Last(i => i > 250));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                Console.WriteLine("Last Even Element in intList: {0}", strList.LastOrDefault(s => s.Contains("T")));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        // The First and FirstOrDefault method returns an element from the zeroth index in the collection
+        // i.e.the first element.Also, it returns an element that satisfies the specified condition.
+        // First Returns the first element of a collection, or the first element that satisfies a condition.
+        // FirstOrDefault Returns the first element of a collection, or the first element that satisfies a condition.Returns a default value if index is out of range.
+        private void FirstOrDefault()
+        {
+            var intList = new List<int>() { 7, 10, 21, 30, 45, 50, 87 };
+            var strList = new List<string>() { null, "Two", "Three", "Four", "Five" };
+            var emptyList = new List<string>();
+
+            Console.WriteLine("1st Element in intList: {0}", intList.First());
+            Console.WriteLine("1st Even Element in intList: {0}", intList.First(i => i % 2 == 0));
+
+            Console.WriteLine("1st Element in strList: {0}", strList.First());
+
+            Console.WriteLine("emptyList.First() throws an InvalidOperationException");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            try
+            {
+                Console.WriteLine(emptyList.First());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("1st Element in intList: {0}", intList.FirstOrDefault());
+            Console.WriteLine("1st Even Element in intList: {0}", intList.FirstOrDefault(i => i % 2 == 0));
+
+            Console.WriteLine("1st Element in strList: {0}", strList.FirstOrDefault());
+
+            Console.WriteLine("1st Element in emptyList: {0}", emptyList.FirstOrDefault());
+
+            // Be careful while specifying condition in First() or FirstOrDefault(). 
+            // First() will throw an exception if a collection does not include any element that satisfies the specified condition or includes null element.
+            try
+            {
+                Console.WriteLine("1st Element which is greater than 250 in intList: {0}", intList.First(i => i > 250));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                Console.WriteLine("1st Even Element in intList: {0}", strList.FirstOrDefault(s => s.Contains("T")));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        // ElementAt Returns the element at a specified index in a collection
+        // ElementAtOrDefault Returns the element at a specified index in a collection or a default value if the index is out of range.
+        private void ElementAt()
+        {
+            var intList = new List<int>() { 10, 21, 30, 45, 50, 87 };
+            var strList = new List<string>() { "One", "Two", null, "Four", "Five" };
+
+            Console.WriteLine("1st Element in intList: {0}", intList.ElementAt(0));
+            Console.WriteLine("1st Element in strList: {0}", strList.ElementAt(0));
+            Console.WriteLine("2nd Element in intList: {0}", intList.ElementAt(1));
+            Console.WriteLine("2nd Element in strList: {0}", strList.ElementAt(1));
+            Console.WriteLine("3rd Element in intList: {0}", intList.ElementAtOrDefault(2));
+            Console.WriteLine("3rd Element in strList: {0}", strList.ElementAtOrDefault(2));
+            Console.WriteLine("10th Element in intList: {0} - default int value", intList.ElementAtOrDefault(9));
+            Console.WriteLine(
+                "10th Element in strList: {0} - default string value (null)",
+                strList.ElementAtOrDefault(9));
+            Console.WriteLine("intList.ElementAt(9) throws an exception: Index out of range");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            try
+            {
+                Console.WriteLine(intList.ElementAt(9));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        // The Sum() method calculates the sum of numeric items in the collection.
+        private void Sum()
+        {
+            var intList = new List<int>() { 10, 21, 30, 45, 50, 87 };
+
+            var total = intList.Sum();
+
+            Console.WriteLine("Sum: {0}", total);
+
+            int sumOfEvenElements = intList.Sum(i => i % 2 == 0 ? i : 0);
+
+            Console.WriteLine("Sum of Even Elements: {0}", sumOfEvenElements);
+
+            var sumOfAge = this.StudentList.Sum(s => s.Age);
+
+            Console.WriteLine("Sum of all student's age: {0}", sumOfAge);
+
+            var numOfAdults = this.StudentList.Sum(s => s.Age >= 18 ? 1 : 0);
+
+            Console.WriteLine("Total Adult Students: {0}", numOfAdults);
+        }
+
+        // The Max operator returns the largest numeric element from a collection.
+        private void Max()
+        {
+            var intList = new List<int>() { 10, 21, 30, 45, 50, 87 };
+
+            var largest = intList.Max();
+
+            Console.WriteLine("Largest Element: {0}", largest);
+
+            var largestEvenElements = intList.Max(i => i % 2 == 0 ? i : 0);
+
+            Console.WriteLine("Largest Even Element: {0}", largestEvenElements);
+
+            var oldest = this.StudentList.Max(s => s.StudentName.Length);
+
+            Console.WriteLine($"Oldest student is: {oldest}");
+        }
+
+        // The Count operator returns the number of elements in the collection 
+        // or number of elements that have satisfied the given condition.
         private void Count()
         {
-            // The Count operator returns the number of elements in the collection 
-            // or number of elements that have satisfied the given condition.
-
             var numOfStudents = this.StudentList.Count();
 
             Console.WriteLine("Number of Students: {0}", numOfStudents);
@@ -73,6 +424,8 @@ namespace LinqTutorial
 
             Console.WriteLine("Number of mature Students: {0}", totalAge);
         }
+
+
 
         // Average extension method calculates the average of the numeric items in the collection.
         // Average method returns nullable or non-nullable decimal, double or float value.
